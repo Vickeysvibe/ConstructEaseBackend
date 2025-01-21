a729ed3283d2f01221a79c492d57bc809f462cca
Suresh Krishna D <130873072+SureshKrishnaD@users.noreply.github.com>
2025-01-21 10:35:14 +0530

    update

--- Routes/auth.routes.js
+++ Routes/auth.routes.js
@@ -4,6 +4,6 @@ import { checkout, login } from "../Controllers/auth.controller";
 const router = express.Router();
 
 router.post("/login", login);
-router.post("checkout",checkout);
+router.post("/checkout",checkout);
 
 export default router;
