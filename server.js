482787c59855fcca2fa7b46cd8ee3571082870e1
kathiroli21 <kathiroli216@gmail.com>
2025-02-08 02:43:53 +0530

    vercel

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
@@ -6,28 +5,23 @@ import dotenv from "dotenv";
 import connectDB from "./Configs/mongoDbConfig.js";
 import { routing } from "./routes.js"; // Import the routing function
 import fileUpload from "express-fileupload";
-import loggerMiddleware from "./Middlewares/logger.middleware.js"
-
-
+import loggerMiddleware from "./Middlewares/logger.middleware.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(loggerMiddleware);
-
-
 // Middleware
+app.use(loggerMiddleware);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-    fileUpload({
-      useTempFiles: false,
-      tempFileDir: "/tmp/",
-    })
-  );
-  
+  fileUpload({
+    useTempFiles: false,
+    tempFileDir: "/tmp/",
+  })
+);
 
 // Connect to DB
 connectDB();
@@ -38,6 +32,5 @@ app.get("/", (req, res) => res.send("API is running perfectly!"));
 // Use the routing function to register routes
 routing(app); // Now this registers all routes
 
-// Listen on a port
-
+// Export the app for Vercel
 export default app;
